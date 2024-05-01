89110f1c24fad89c2741503a8e3c1844ffb0bfa9
Kader ZKR <abdelkader.zekraoui1@gmail.com>
2024-05-02 00:41:05 +0100

    Animated the bird & the sky ^^

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import { useRef, useEffect } from "react";
+import { useAnimations, useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 
 import birdScene from "../assets/3d/bird.glb";
-import { useGLTF } from "@react-three/drei";
 
 const Bird = () => {
+  const birdRef = useRef();
   const { scene, animations } = useGLTF(birdScene);
+  const { actions } = useAnimations(animations, birdRef);
+
+  useEffect(() => {
+    actions["Take 001"].play();
+  }, []);
+
+  useFrame((_, delta) => {
+    birdRef.current.rotation.y += 0.15 * delta;
+  });
 
   return (
-    <mesh position={[-3, 1, 1]} scale={[0.003, 0.003, 0.003]}>
+    <mesh position={[-3, 1, 1]} scale={[0.003, 0.003, 0.003]} ref={birdRef}>
       <primitive object={scene} />
     </mesh>
   );

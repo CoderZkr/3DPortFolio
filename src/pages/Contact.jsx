3b41dc1020c4a954ec03bd7ddd6c4b0fc1bd5a04
Kader ZKR <abdelkader.zekraoui1@gmail.com>
2024-05-04 01:02:06 +0100

    Created Alert sendin

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,6 +3,7 @@ import emailjs from "@emailjs/browser";
 import { Canvas } from "@react-three/fiber";
 import Fox from "../models/Fox";
 import useAlert from "../hooks/useAlert";
+import Alert from "../components/Alert";
 
 import Loader from "../components/Loader";
 
@@ -68,6 +69,9 @@ const Contact = () => {
 
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
+      {alert.show && <Alert {...alert} />}
+      <Alert text="test" />
+
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Get in Touch</h1>
         <form

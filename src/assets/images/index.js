d9a0893a330d8dabff0d10fe78f2a730e3e530c1
Kader ZKR <abdelkader.zekraoui1@gmail.com>
2024-05-05 19:34:21 +0100

    Updated the About page, added skills and experiences

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,15 +1,25 @@
-import hero from './hero.jpg'
-import meta from './meta.png'
-import shopify from './shopify.png'
-import starbucks from './starbucks.png'
-import tesla from './tesla.png'
-import logo from './logo.svg'
+import hero from "./hero.jpg";
+import meta from "./meta.png";
+import shopify from "./shopify.png";
+import starbucks from "./starbucks.png";
+import tesla from "./tesla.png";
+import logo from "./logo.svg";
+import algeriapartners from "./algeriapartners.jpg";
+import algposte from "./algposte.jpg";
+import gica from "./gica.png";
+import mfg from "./mfg.jpg";
+import ooredoo from "./ooredoo.jpg";
 
 export {
-    hero,
-    meta,
-    shopify,
-    starbucks,
-    tesla,
-    logo
-}
+  ooredoo,
+  algeriapartners,
+  algposte,
+  gica,
+  mfg,
+  hero,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  logo,
+};
